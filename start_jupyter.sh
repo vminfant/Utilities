935b109975072a1aed2421570ffb6afa26ff4bf3
Infant Alex <vminfant@gmail.com>
2019-05-30 08:00:47 +0800

    Added an additional check to make sure only one screen is active

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,1 +1,16 @@
-screen -m -d -S "Jupyter-screen" jupyter notebook
+screen -list | grep "Jupyter-screen" > /dev/null
+sts=`echo $?`
+
+if [ $sts -ne 0 ] 
+then
+	screen -m -d -S "Jupyter-screen" jupyter notebook
+
+	echo ""
+	echo "Jupyter notebook is successfully started"
+	echo ""
+else
+	echo ""
+	echo "Jupyter notebook is already running!"
+	echo ""
+fi
+
