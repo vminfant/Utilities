5641fc6a45c0aff02779550a06dba456262b673f
Infant Alex <vminfant@gmail.com>
2019-05-31 09:37:39 +0800

    Added additional checks for stop

--- stop_jupyter.sh
+++ stop_jupyter.sh
@@ -1,1 +1,15 @@
-screen -S "Jupyter-screen" -X kill 
+screen -list | grep "Jupyter-screen" > /dev/null
+sts=`echo $?`
+
+if [ $sts -eq 0 ] 
+then
+    screen -S "Jupyter-screen" -X kill 
+
+    echo ""
+    echo "Jupyter notebook is successfully stopped!"
+    echo ""
+else
+    echo ""
+    echo "There is no active Jupyter notebook screen found!"
+    echo ""
+fi
